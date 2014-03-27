dfc21b5bd076d9e48e042cd84272f1dd6970aa2d
Mitchell Tee <c7e8@deas.ugrad.cs.ubc.ca>
2014-03-27 10:40:57 -0700

    There must be a better way.

--- fixpermissions.sh
+++ fixpermissions.sh
@@ -2,4 +2,10 @@
 
 find . -name "*.js" -exec chmod 644 {} ";"
 find . -name "*.html" -exec chmod 644 {} ";"
+find . -name "*.png" -exec chmod 644 {} ";"
+find . -name "*.gif" -exec chmod 644 {} ";"
+find . -name "*.css" -exec chmod 644 {} ";"
+find . -name "*.ttf" -exec chmod 644 {} ";"
+find . -name "*.woff" -exec chmod 644 {} ";"
+find . -name "*.svg" -exec chmod 644 {} ";"
 find . -type d -exec chmod 755 {} ";"
